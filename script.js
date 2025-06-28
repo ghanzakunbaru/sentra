dbf9af3c0ee1510a6d15787d235d4fc55ddf827c
Ghanz <akungabut1zf@gmail.com>
2025-06-28 15:44:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+async function login() {
+  const user = document.getElementById("username").value.trim();
+  const pass = document.getElementById("password").value.trim();
+  const status = document.getElementById("status");
+
+  if (!user || !pass) {
+    return status.textContent = "⚠️ Lengkapi semua kolom!";
+  }
+
+  // Ganti URL ini ke RAW GitHub kamu
+  const dataURL = "https://raw.githubusercontent.com/ghanzakunbaru/payment/main/users.json";
+
+  try {
+    const res = await fetch(dataURL);
+    const data = await res.json();
+
+    if (data[user] && data[user] === pass) {
+      // Simpan ke sessionStorage biar bisa akses dashboard
+      sessionStorage.setItem("user", user);
+      window.location.href = "dashboard.html";
+    } else {
+      status.textContent = "❌ Username atau password salah!";
+    }
+  } catch (e) {
+    status.textContent = "⚠️ Gagal ambil data user!";
+  }
+}
